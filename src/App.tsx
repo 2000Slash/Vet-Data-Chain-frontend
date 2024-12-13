160259c4dc4aac770d713995692095fd6dd3d8af
unknown <seeliger.andreas@web.de>
2024-12-13 09:49:37 +0100

    fixed a stupid idea in my app.tsx now everything about state is in the store fiel

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,9 @@ import React, { createContext, useContext, useState } from 'react';
 import Login from "./pages/login_page";
 import Vetenary_office from "./pages/vetenary_office_page"
 import Farmer from "./pages/farmer_page";
+import useStore from './store.tsx'
 
-import  from "zustand";
 
-export const useKeeperWalletStore = create((set) => ({
-  keeperWalletState: null, // Initial state for Keeper Wallet data
-  setKeeperWalletState: (state) => set({ keeperWalletState: state }), // Action to update the state
-}));
 const App: React.FC = () => (
   <Router>
     <Routes>
