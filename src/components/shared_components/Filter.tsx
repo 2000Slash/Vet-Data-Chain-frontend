d7b3aa402c9c2a3564374a08329a7480dfeed3f4
Leon <leon.kastenholz@gmail.com>
2025-01-18 14:11:29 +0100

    optimized the filter

--- src/components/shared_components/Filter.tsx
+++ src/components/shared_components/Filter.tsx
@@ -2,7 +2,15 @@ import * as React from "react";
 import "../../styles/details/Filter.css";
 import { getAllTableNamess, getAllFields } from "../../utils/sqlRequests";
 
-const Filter = ({ onFilterSubmit }: { onFilterSubmit: (selectedTable: string, selectedAttribute: string, inputValue: string) => void }) => {
+const Filter = ({
+  onFilterSubmit,
+}: {
+  onFilterSubmit: (
+    selectedTable: string,
+    selectedAttribute: string,
+    inputValue: string
+  ) => void;
+}) => {
   const [openFirstFilter, setOpenFirstFilter] = React.useState(false);
   const [openSecondFilter, setOpenSecondFilter] = React.useState(false);
   const [openInputFilter, setOpenInputFiler] = React.useState(false);
@@ -12,6 +20,7 @@ const Filter = ({ onFilterSubmit }: { onFilterSubmit: (selectedTable: string, se
   const [fieldNames, setFieldNames] = React.useState<{ name: string }[]>([]);
   const [inputValue, setInputValue] = React.useState("");
   const [secondMenuVisible, setSecondMenuVisible] = React.useState(false);
+  const [appliedFilters, setAppliedFilters] = React.useState<{table: string, attribute: string; value: string }[]>([]);
 
   const handleOpenFirstFilter = async () => {
     setOpenFirstFilter(!openFirstFilter);
@@ -59,80 +68,116 @@ const Filter = ({ onFilterSubmit }: { onFilterSubmit: (selectedTable: string, se
 
   const handleFilterClick = () => {
     if (selectedTable !== "Table ID" && selectedAttribute && inputValue) {
-      onFilterSubmit(selectedTable, selectedAttribute, inputValue); 
+
+      const newFilter = { table: selectedTable, attribute: selectedAttribute, value: inputValue };
+      setAppliedFilters((prevFilters) => [...prevFilters, newFilter]);
+
+    
+      const formattedFilters = appliedFilters.map(filter => [filter.table, filter.attribute, filter.value]);
+      console.log(formattedFilters);
+
+      onFilterSubmit(selectedTable, selectedAttribute, inputValue);
+
+      setSecondMenuVisible(false);
+      setOpenInputFiler(false);
+      setSelectedTable("Table ID");
+      setSelectedAttribute("");
+      setInputValue("");
     } else {
       alert("Please fill in all fields.");
     }
   };
 
   const handleResetClick = () => {
-    onFilterSubmit("","","");
+    onFilterSubmit("", "", "");
     setSecondMenuVisible(false);
     setOpenInputFiler(false);
     setSelectedTable("Table ID");
     setSelectedAttribute("");
     setInputValue("");
+    setAppliedFilters([]);
   };
 
   return (
-    <div className="filter-container">
-      <div className="dropdown">
-        <button className="menu-button" onClick={handleOpenFirstFilter}>
-          {selectedTable}
-        </button>
-        {openFirstFilter ? (
-          <ul className="menu">
-            {tableNames.map((table, index) => (
-              <li key={index} className="menu-item">
-                <button
-                  className="menu-button"
-                  onClick={() => handleButtonInFirstFilter(table.name)}
-                >
-                  {table.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-        ) : null}
-      </div>
-
-      {secondMenuVisible && (
+    <>
+      <div className="filter-container">
         <div className="dropdown">
-          <button className="menu-button" onClick={handleOpenSecondFilter}>
-            {selectedAttribute || selectedTable}
+          <button className="menu-button" onClick={handleOpenFirstFilter}>
+            {selectedTable}
           </button>
-          {openSecondFilter ? (
+          {openFirstFilter ? (
             <ul className="menu">
-              {fieldNames.map((field, index) => (
+              {tableNames.map((table, index) => (
                 <li key={index} className="menu-item">
                   <button
                     className="menu-button"
-                    onClick={() => handleButtonInSecondFilter(field.name)}
+                    onClick={() => handleButtonInFirstFilter(table.name)}
                   >
-                    {field.name}
+                    {table.name}
                   </button>
                 </li>
               ))}
             </ul>
           ) : null}
         </div>
-      )}
 
-      {selectedTable !== "Table ID" && selectedAttribute && openInputFilter && (
-        <div className="input-field-container">
-          <input
-            id="inputField"
-            className="input-field"
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Type your filter text"
-          />
-          <button className="input-button" onClick={handleFilterClick}>Filter</button>
-          <button className="input-button" onClick={handleResetClick}>Reset</button>
+        {secondMenuVisible && (
+          <div className="dropdown">
+            <button className="menu-button" onClick={handleOpenSecondFilter}>
+              {selectedAttribute || selectedTable}
+            </button>
+            {openSecondFilter ? (
+              <ul className="menu">
+                {fieldNames.map((field, index) => (
+                  <li key={index} className="menu-item">
+                    <button
+                      className="menu-button"
+                      onClick={() => handleButtonInSecondFilter(field.name)}
+                    >
+                      {field.name}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
+        )}
+
+        {selectedTable !== "Table ID" &&
+          selectedAttribute &&
+          openInputFilter && (
+            <div className="input-field-container">
+              <input
+                id="inputField"
+                className="input-field"
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Type your filter text"
+              />
+              <button className="input-button" onClick={handleFilterClick}>
+                Filter
+              </button>
+             
+            </div>
+          )}
+      </div>
+      
+        <div className="appliedFilters-container">
+          <h3>Applied Filters</h3>
+          <ul className="appliedFilters-list">
+            {appliedFilters.map((filter, index) => (
+              <li key={index}>
+                Attribute: {filter.attribute}, Value: {filter.value}
+              </li>
+            ))}
+          </ul>
+          <button className="input-button" onClick={handleResetClick}>
+            Reset
+          </button>
         </div>
-      )}
-    </div>
+      
+    </>
   );
 };
 
