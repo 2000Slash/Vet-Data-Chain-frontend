5088eb92de9ed1630db2bfa7bb2db879fe9bf5ae
NIMAR <ramin.saeedi1992@gmail.com>
2025-02-19 12:46:40 +0100

    add Table pagination

--- src/components/shared_components/Table.tsx
+++ src/components/shared_components/Table.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../../styles/details/Table.css";
 import DetailInformationModal from "./DetailInformationModal";
 
@@ -35,19 +35,31 @@ interface TableProps {
   };
 }
 const Table: React.FC<TableProps> = ({ jsonData }) => {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedRecord, setSelectedRecord] = useState<AaDRecord | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = jsonData.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(jsonData.length / itemsPerPage);
+  const [pageInput, setPageInput] = useState("");
+
+  const handleDirectPageGo = () => {
+    const pageNumber = parseInt(pageInput);
+    if (pageNumber && pageNumber > 0 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+      setPageInput("");
+    }
+  };
 
   const openModal = (record: AaDRecord) => {
-    console.log("Modal open");
-    console.log(jsonData);
     setSelectedRecord(record);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
-    console.log("Modal closed");
     setSelectedRecord(null);
     setIsModalOpen(false);
   };
@@ -57,13 +69,11 @@ const Table: React.FC<TableProps> = ({ jsonData }) => {
       <table className="table">
         <thead>
           <tr>
+            <th scope="col">Number of Animals,Animal IDs, Species, Weight</th>
+            <th scope="col">Diagnosis, Diagnosis date</th>
             <th scope="col">
-              Number of Animals,Animal IDs, Species, Weight
+              Medication name, Active ingredient, Pharmaceutical form
             </th>
-            <th scope="col">
-              Diagnosis, Diagnosis date
-            </th>
-            <th scope="col">Medication name, Active ingredient, Pharmaceutical form </th>
             <th scope="col">Batch name</th>
             <th scope="col">Application amount</th>
             <th scope="col">Dosage per animal and day</th>
@@ -74,35 +84,80 @@ const Table: React.FC<TableProps> = ({ jsonData }) => {
           </tr>
         </thead>
         <tbody>
-        {jsonData.map((record: any, recordIndex: number) => (
-          <tr key={recordIndex}>
-            <td>
-              {record.numberOfAnimals}, {record.animalIDS}, {record.species}, {record.weight}kg
-            </td>
-            <td>
-              {record.diagnosis}, {record.diagnosisDate.split("-").reverse().join(".")}
-            </td>
-            <td>
-              {record.medicationName}, {record.activeIngredient}, {record.pharmaceuticalForm}
-            </td>
-            <td>{record.batchName}</td>
-            <td>{record.applicationAmount}</td>
-            <td>{record.dosagePerAnimalDay}</td>
-            <td>{record.routeOfAdministration}</td>
-            <td>{record.durationAndTiming}</td>
-            <td>
-              {record.withdrawalEdible}, {record.withdrawalMilk}, {record.withdrawalEggs}, {record.withdrawalHoney}
-            </td>
-            <td>
-              {record.treatmentDays}, {record.effectiveDays}
-            </td>
-            <td className="button-cell">
-              <button className="button" onClick={() => openModal(record)}>Details</button>
-            </td>
-          </tr>
-      ))}
-</tbody>
+          {currentItems.map((record: any, recordIndex: number) => (
+            <tr key={recordIndex}>
+              <td>
+                {record.numberOfAnimals}, {record.animalIDS}, {record.species},{" "}
+                {record.weight}kg
+              </td>
+              <td>
+                {record.diagnosis},{" "}
+                {record.diagnosisDate.split("-").reverse().join(".")}
+              </td>
+              <td>
+                {record.medicationName}, {record.activeIngredient},{" "}
+                {record.pharmaceuticalForm}
+              </td>
+              <td>{record.batchName}</td>
+              <td>{record.applicationAmount}</td>
+              <td>{record.dosagePerAnimalDay}</td>
+              <td>{record.routeOfAdministration}</td>
+              <td>{record.durationAndTiming}</td>
+              <td>
+                {record.withdrawalEdible}, {record.withdrawalMilk},{" "}
+                {record.withdrawalEggs}, {record.withdrawalHoney}
+              </td>
+              <td>
+                {record.treatmentDays}, {record.effectiveDays}
+              </td>
+              <td className="button-cell">
+                <button className="button" onClick={() => openModal(record)}>
+                  Details
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
+
+      <div className="pagination">
+        <div className="page-input-container">
+          <button
+            onClick={handleDirectPageGo}
+            className="pagination-button"
+            disabled={!pageInput}
+          >
+            Go
+          </button>
+          <input
+            type="number"
+            value={pageInput}
+            onChange={(e) => setPageInput(e.target.value)}
+            className="page-input"
+            min="1"
+            max={totalPages}
+          />
+        </div>
+        <button
+          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+          className="pagination-button"
+        >
+          Previous
+        </button>
+        <span>
+          {currentPage} / {totalPages}
+        </span>
+        <button
+          onClick={() =>
+            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+          }
+          disabled={currentPage === totalPages}
+          className="pagination-button"
+        >
+          Next
+        </button>
+      </div>
 
       {isModalOpen && selectedRecord && (
         <DetailInformationModal
@@ -111,14 +166,11 @@ const Table: React.FC<TableProps> = ({ jsonData }) => {
           signatureId={selectedRecord.signatureId}
           contactDataVetenaryId={selectedRecord.contactDataVetenaryId}
           contactDataFarmerId={selectedRecord.contactDataFarmerId}
-          dateOfIssueId={selectedRecord.dateOfIssueId}        />
+          dateOfIssueId={selectedRecord.dateOfIssueId}
+        />
       )}
     </div>
   );
 };
 
 export default Table;
-
-
-
-
